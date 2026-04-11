2fe132092aa70750b499996c21eb8d64928711a7
David Reinhold <david.reinhold@cellebrite.com>
2026-04-11 22:39:47 +0300

    Add Google Services plugin and Kotlin Android plugin to build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,8 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.jetbrains.kotlin.android)
+    id("com.google.gms.google-services")
 }
 
 android {
