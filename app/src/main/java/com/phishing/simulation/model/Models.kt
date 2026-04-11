a743c9b453f66307a765d3badc0d363b8eb27a6d
david-reinhold-1 <davreinholdgpt@gmail.com>
2026-04-12 02:34:34 +0300

    fix: standardize property names in User model

--- app/src/main/java/com/phishing/simulation/model/Models.kt
+++ app/src/main/java/com/phishing/simulation/model/Models.kt
@@ -33,8 +33,8 @@ data class User(
     @set:PropertyName("fcmToken")
     var fcmToken: String = "",
 
-    @get:PropertyName("createdAt")
-    @set:PropertyName("createdAt")
+    @get:PropertyName("CreatedAt")
+    @set:PropertyName("CreatedAt")
     var createdAt: Timestamp = Timestamp.now()
 )
 
